d1070b0c1c0d4e2bcca55733d183e968b8e0da43
Swann Bourgeois <swann.bourgeois2402@gmail.com>
2022-01-09 19:20:16 +0100

    Génération de formations et d'entreprises

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,15 +4,57 @@ namespace App\DataFixtures;
 
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+
+use App\Entity\Entreprise;
 use App\Entity\Formation;
+use App\Entity\Stage;
+
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
+        //Création d'un générateur de données Faker
+        $faker = \Faker\Factory::create('fr_FR');
+
+
+        //Création des formations
         $DUTInfo = new Formation();
         $DUTInfo->setNomLong("DUT Informatique");
         $DUTInfo->setNomCourt("DUT Info");
-        $manager->persist($DUTInfo);
+        
+        $LPProg = new Formation();
+        $LPProg->setNomLong("Licence Professionnelle Programmation Avancée");
+        $LPProg->setNomCourt("LP Prog");
+
+        $LPNum = new Formation();
+        $LPNum->setNomLong("Licence Professionnelle Métiers du Numérique");
+        $LPNum->setNomCourt("LP Num");
+
+        $LInfo = new Formation();
+        $LInfo->setNomLong("Licence Informatique");
+        $LInfo->setNomCourt("L Info");
+
+        $tabFormations = array($DUTInfo,$LPProg,$LPNum,$LInfo);
+        foreach($tabFormations as $tab)
+        {
+            $manager->persist($tab);
+        }
+
+        //Création des Entreprises
+            $tabEntreprise = array();
+            for($i = 0; $i <15; $i++)
+            {
+                $Entreprise = new Entreprise();
+                $Entreprise->setNom($faker->company);
+                $Entreprise->setAdresse($faker->address);
+                $Entreprise->setActivite($faker->realText($maxNbChars = 50, $indexSize = 2));
+                $Entreprise->setURLsite($faker->domainName);
+
+                array_push($tabEntreprise,$Entreprise);
+                $manager->persist($Entreprise);
+            }
+        
+
 
         $manager->flush();
     }
